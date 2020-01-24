0fb9255fec687290d8ad5cb985f8352be1c36017
Emmanuel <emmanuel.roberson3@gmail.com>
2020-01-23 19:23:57 -0600

    gitignore, working out structure of inventory system
    Added base object holder struct, added storage and distribute interfaces, added conditional struct for sorting

---
+++ Inventory Project/Assets/Script/TestBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+using Object = System.Object;
+
+public class TestBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+interface IHoardable
+{
+    void Collect<T>(params T[] collectedObjects);
+}
+
+interface IDistributable
+{
+    void DistributeByType<T>(params IHoardable[] hoardTypes);
+    
+    void DistributeAllTo();
+
+    void DistributeBySort(ConditionTypeList conditionTypes);
+}
+
+struct TransferableObject
+{
+    private UnityEngine.Object objectData;
+    
+    public UnityEngine.Object ObjectData
+    {
+        get => objectData;
+        set => objectData = value;
+    }
+}
+
+class ObjectStorage<T> : IHoardable
+{
+    private List<T> hoardedObjects;
+    
+    public ObjectStorage(params T[] initialObjects)
+    {
+        hoardedObjects = new List<T>(initialObjects);
+    }
+
+    public void Collect<T>(params T[] collectedObject)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+struct ConditionTypeList
+{
+    public List<object> conditionalTypes;
+}
+
+class ObjectDispenser : IDistributable
+{
+
+    void TestFunc()
+    {
+        
+    }
+
+    public void DistributeByType<T>(params IHoardable[] hoardTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DistributeAllTo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DistributeBySort(ConditionTypeList conditionTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+}
